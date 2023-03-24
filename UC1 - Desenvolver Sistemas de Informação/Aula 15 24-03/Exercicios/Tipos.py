88dfafdb22a9a345afbf194e3e5991f88fb402ee
eCrMarques <126702522+eCrMarques@users.noreply.github.com>
2023-03-24 16:58:39 -0300

    Update Tipos.py

--- UC1 - Desenvolver Sistemas de Informação/Aula 15 24-03/Exercicios/Tipos.py
+++ UC1 - Desenvolver Sistemas de Informação/Aula 15 24-03/Exercicios/Tipos.py
@@ -1,4 +1,6 @@
-aVant=['Agua',{'Vantagem':'Fogo','Desvantagem':'Planta'},
+aVant=['Agua',{'Vantagem':['Fogo','a'],'Desvantagem':'Planta'},
        'Planta',{'Vantagem':'Agua','Desvantagem':'Fogo'},
        'Fogo',{'Vantagem':'Planta','Desvantagem':'Agua'}]
 
+if 'a' in aVant[1]['Vantagem']:
+    print('a')
